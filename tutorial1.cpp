cf5f5fae2acb48c90a0dbf5c6086babf04d1b897
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2017-03-19 23:23:32 +0100

    First commit of c++ tutorials

---
+++ tutorial1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+  int ctab[5] = {1,2,3,4,5};
+  // Write a loop to print all elements on the screen.
+  cout << ctab[0] << endl;
+
+  std::vector<int> vectOfInts= {1,2,3,4,5}; 
+  cout << vectOfInts[1] << endl;
+  // Write a loop to print all elements on the screen.
+  
+  return 0;
+}
