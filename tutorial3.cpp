7e016123291a656daad972f44f1b587e75d76e65
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2017-03-20 12:14:14 +0100

    Add map tutorial3

---
+++ tutorial3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+int main()
+{
+  // 1.Create a map int to double, init it with values 1 : 2.5,  4 : 5.3, 3 : 6.5, 2: -10
+  // 2a.Print the content of the map on the screen
+  // 2c. Print 3rd element on the screen
+  // 2d. Change the content of the 3th element to 100
+  // 3.Add elements 5 -> 10.2  
+  // 5.Print the size of the map 
+  return 0;
+}
