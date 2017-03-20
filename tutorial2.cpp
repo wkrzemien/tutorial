b7d9547c99bf4cabe81dc6bfe9ba5f5bffa344bb
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2017-03-20 10:23:40 +0100

    Add tutorial2

---
+++ tutorial2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+  // 1.Create a vector of integers vec1 with 5,4,3,2,1
+  // 2.Print the content of the vector on screen
+  // 2b.Printe the content in the reverse order.
+  // 3.Add elements 5,6,7,8,5  to the end of the vector push_back()
+  // 4.Print the first and the last of the element front(), back()
+  // 5.Print the size of the vector
+  // 6.Find the smallest element min()
+  // 7.Find the largest element max()
+  // 8.Count number of appearence of 5
+  // 9.Sort the vector
+  // 10.Print its content on the screen
+  // 11.Create another vector vec2
+  // 12.Check if it is empty
+  // 13.Add 20, 22 to second vector
+  // 14.Insert 23 inbetween 20 and 23.
+  // 15.copy first 5 elements from vec1 into vec3
+  // 16.Merge vec2 and vec3.
+  return 0;
+}
