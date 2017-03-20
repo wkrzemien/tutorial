26977e888368b51ebf49e476ad6cc779ace33c0e
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2017-03-20 12:03:39 +0100

    Change tutorial elements

--- tutorial2.cpp
+++ tutorial2.cpp
@@ -5,13 +5,15 @@ using namespace std;
 int main()
 {
   // 1.Create a vector of integers vec1 with 5,4,3,2,1
-  // 2.Print the content of the vector on screen
-  // 2b.Printe the content in the reverse order.
+  // 2a.Print the content of the vector on screen
+  // 2b.Print the content in the reverse order.
+  // 2c. Print 3rd element on the screen
+  // 2d. Change the content of the 4th element to 100
   // 3.Add elements 5,6,7,8,5  to the end of the vector push_back()
   // 4.Print the first and the last of the element front(), back()
   // 5.Print the size of the vector
-  // 6.Find the smallest element min()
-  // 7.Find the largest element max()
+  // 6.Find the smallest element min_element()
+  // 7.Find the largest element max_element()
   // 8.Count number of appearence of 5
   // 9.Sort the vector
   // 10.Print its content on the screen
@@ -20,6 +22,5 @@ int main()
   // 13.Add 20, 22 to second vector
   // 14.Insert 23 inbetween 20 and 23.
   // 15.copy first 5 elements from vec1 into vec3
-  // 16.Merge vec2 and vec3.
   return 0;
 }
