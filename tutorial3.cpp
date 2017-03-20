110d3f3314031f4ebd53c83c5f7db9280b01a4ec
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2017-03-20 23:28:53 +0100

    Add more problems to tutorial3 and add tutorial4 for std::string

--- tutorial3.cpp
+++ tutorial3.cpp
@@ -4,11 +4,16 @@
 using namespace std;
 int main()
 {
-  // 1.Create a map int to double, init it with values 1 : 2.5,  4 : 5.3, 3 : 6.5, 2: -10
+  // 1.Create a map int to double, init it with values 1 -> 2.5,  4 -> 5.3, 3 -> 6.5, 2 -> -10
   // 2a.Print the content of the map on the screen
-  // 2c. Print 3rd element on the screen
+  // 2c. Print 3rd element on the screen (key and value)
   // 2d. Change the content of the 3th element to 100
-  // 3.Add elements 5 -> 10.2  
-  // 5.Print the size of the map 
+  // 3. Add elements 5 -> 10.2  
+  // 4. Print the size of the map 
+  // 5. Find if the key 10 exists
+  // 6 Create unordered map of std::string to double
+  // 7. add values of "c" -> 10 "a" -> 2.5 , "b" -> 3.4 
+  // 8. display on the screen 
+  // 9. remove element "b"
   return 0;
 }
---
+++ tutorial4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+int main()
+{
+  int k = 4;
+  double m = 4.3;
+  // 1. transform k and m to std::string
+  // 2. create std::string  testStr with "I love c++" content
+  // 3. print testStr length on the screen
+  // 4. save testStr as c-string (const char*) called testCStr
+  // 5. print its content on the screen strlen()
+  // 6. sort testStr 
+  // 7. print 2nd element of the testStr
+  // 8. find occurence of +
+  // 9. replace "++" by "--"
+  // 10. concatenate  testStr with "how are you" string
+  return 0;
+}
